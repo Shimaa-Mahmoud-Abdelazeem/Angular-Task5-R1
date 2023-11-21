c30fc89e69f155529eb67df5446be33cac879bf4
Shimaa-Mahmoud-Abdelazeem <142457787+Shimaa-Mahmoud-Abdelazeem@users.noreply.github.com>
2023-11-21 02:47:27 +0200

    Update app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { CommonModule } from '@angular/common';
   imports: [
     BrowserModule,
     CommonModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
